23e8419f27d461bee287f58d3cec8a597c3f74ad
Vinicius Cenci <cencifabris@gmail.com>
2023-05-04 16:38:59 -0300

    Remove imports inutilizados

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import java.text.NumberFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
         InterfaceDeUsuario ui = new InterfaceDeUsuario();
